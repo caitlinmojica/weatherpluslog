42417bbf5ef5e553b83a6c35cc3a4be1b6449f1c
caitlinmojica <63633829+caitlinmojica@users.noreply.github.com>
2020-10-20 18:04:03 +1300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 """
-BeeApp Weather Station Project 2020
-
-By Caitlin Mojica (16947371)
-
-Last Edit: 16/10/2020
-
-Special thanks to Ramon and Nidhi for helping me code this shit.
-Mostly Ramon for dedicating a lot of his time and mental capacity to help me.
-Also Jeff, who has been there since the beginning, for pushing me to through this,
-and for gathering all the resources and people I needed for this project.
-
-Parts of this code was based off the micro:climate kit experiments from sparkfun.
+BeeApp Weather Station Project 2020 By Caitlin Mojica (16947371) Last Edit: 16/10/2020
 """
 
 current_WindDirection_List = ""
@@ -19,41 +8,28 @@ current_rain = 0
 serial.redirect_to_usb()
 weatherbit.start_wind_monitoring()
 weatherbit.start_rain_monitoring()
+current_time = 0
+
 
 header = ""
 logging_data = 0
 logging_data = -1
 
 basic.show_string("ON")
-#pause(1000)
 
 while logging_data == -1:
     weatherbit.start_weather_monitoring()
     serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE115200)
-    header = "Time" + "," + "Wind Speed" + "," + "Wind Direction" + "," + "Temperature" + "," + "Humidity" + "," + "Pressure" + "," + "Rain Level"
+    header = "Time" + "," + "Wind Speed" + "," + "Wind Direction" + "," + "Temperature" + "," + "Humidity" + "," + "Rain Level"
     pause(100)
     serial.write_line(header)
     logging_data = 0
 
 def on_forever():
-    # Added rain
-    global current_WindSpeed, current_WindDirection_List, current_rain
-
     weatherbit.start_weather_monitoring()
-    current_WindSpeed = (Math.round(weatherbit.wind_speed()))
-    current_WindDirection_List = weatherbit.wind_direction() 
-    current_temp = (Math.idiv(weatherbit.temperature(), 100))
-    current_humidity = (Math.idiv(weatherbit.humidity(), 1024))
-    current_pressure = (Math.idiv(weatherbit.pressure(), 25600))
-    current_rain = (Math.round(weatherbit.rain()))
-
-    #Pauses execution of the function for the specified ms
     pause(1000) 
-    #For troubleshooting purposes, so we can see how long it takes per run
-    basic.show_string("TT")
-    #Board wouldn't write to the SD card if this line isn't here
     serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE9600)
-    # If "???" is displayed, wind direction is unknown!
-    serial.write_line(str(input.running_time()) + "," + str(current_WindSpeed) + "," + str(current_WindDirection_List) + "," + str(current_temp) + "," + str(current_humidity) + "," + str(current_pressure) + "," + str(current_rain) + "\n")
+    serial.write_line(str(input.running_time()) + "," + str((Math.round(weatherbit.wind_speed()))) + "," + str(weatherbit.wind_direction()) + "," + str((Math.idiv(weatherbit.temperature(), 100))) + "," + str((Math.idiv(weatherbit.humidity(), 1024))) + "," + str((Math.round(weatherbit.rain()))) + "\n")
+    pause(1000)
 
 basic.forever(on_forever)
