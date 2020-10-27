c05d6dff45f4a43418365b74fbdaecbba75dc06a
caitlinmojica <63633829+caitlinmojica@users.noreply.github.com>
2020-10-27 16:53:03 +1300

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 """
-BeeApp Weather Station Project 2020 By Caitlin Mojica (16947371) Last Edit: 21/10/2020
+
+BeeApp Weather Station Project 2020 By Caitlin Mojica (16947371) Last Edit: 27/10/2020
+
 """
 
-current_WindDirection_List = ""
-current_WindSpeed = 0
-current_rain = 0
-serial.redirect_to_usb()
 current_time = 0
-
-header = ""
-logging_data = 0
+serial.redirect_to_usb()
 logging_data = -1
-
 basic.show_string("ON")
 
 while logging_data == -1:
     serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE115200)
-    serial.write_line( "Time" + "," + "Wind Speed" + "," + "Wind Direction" + "," + "Temperature" + "," + "Humidity" + "," + "Rain Level")
+    serial.write_line("Time" + "," + "Wind Speed" + "," + "Wind Direction" + "," + "Temperature" + "," + "Humidity" + "," + "Rain Level")
     logging_data = 0
 
 def on_forever():
@@ -24,9 +19,8 @@ def on_forever():
     weatherbit.start_wind_monitoring()
     weatherbit.start_rain_monitoring()
     basic.show_string("LOG")
-    pause(1000) 
-    serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE115200)
-    serial.write_line(str(input.running_time()) + "," + str((Math.round(weatherbit.wind_speed()))) + "," + str(weatherbit.wind_direction()) + "," + str((Math.idiv(weatherbit.temperature(), 100))) + "," + str((Math.idiv(weatherbit.humidity(), 1024))) + "," + str((Math.round(weatherbit.rain()))) + "\n")
     pause(1000)
-
+    serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE115200)
+    serial.write_line("" + str(input.running_time()) + "," + ("" + str(Math.round(weatherbit.wind_speed()))) + "," + ("" + weatherbit.wind_direction()) + "," + ("" + str(Math.idiv(weatherbit.temperature(), 100))) + "," + ("" + str(Math.idiv(weatherbit.humidity(), 1024))) + "," + ("" + str(Math.round(weatherbit.rain()))) + "\n")
+    pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,12 +1,7 @@
-/** BeeApp Weather Station Project 2020 By Caitlin Mojica (16947371) Last Edit: 21/10/2020 */
-let current_WindDirection_List = ""
-let current_WindSpeed = 0
-let current_rain = 0
-serial.redirectToUSB()
+/** BeeApp Weather Station Project 2020 By Caitlin Mojica (16947371) Last Edit: 27/10/2020 */
 let current_time = 0
-let header = ""
-let logging_data = 0
-logging_data = -1
+serial.redirectToUSB()
+let logging_data = -1
 basic.showString("ON")
 while (logging_data == -1) {
     serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate115200)
@@ -20,6 +15,6 @@ basic.forever(function on_forever() {
     basic.showString("LOG")
     pause(1000)
     serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate115200)
-    serial.writeLine("" + input.runningTime() + "," + ("" + Math.round(weatherbit.windSpeed())) + "," + ("" + weatherbit.windDirection()) + "," + ("" + Math.idiv(weatherbit.temperature(), 100)) + "," + ("" + Math.idiv(weatherbit.humidity(), 1024)) + "," + ("" + Math.round(weatherbit.rain())) + "\n")
+    serial.writeLine("" + ("" + input.runningTime()) + "," + ("" + ("" + Math.round(weatherbit.windSpeed()))) + "," + ("" + weatherbit.windDirection()) + "," + ("" + ("" + Math.idiv(weatherbit.temperature(), 100))) + "," + ("" + ("" + Math.idiv(weatherbit.humidity(), 1024))) + "," + ("" + ("" + Math.round(weatherbit.rain()))) + "\n")
     pause(1000)
 })
